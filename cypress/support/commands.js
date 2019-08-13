c2c9fd25a78fc2ab484726cc5f8ab396341f3193
bpeterson1663 <bpeterson1663@gmail.com>
2019-08-12 20:15:43 -0500

    add sign up and login commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,18 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (email, password) => {
+    cy.get('[type="email"]')
+        .type(email).should('have.value', email);
+    cy.get('[type="password"]')
+        .type(password).should('have.value', password);   
+    cy.get('[type="submit"]')
+        .click();
+});
+
+Cypress.Commands.add('signup', (email, password) => {
+    cy.get('[type="email"]')
+        .type(email).should('have.value', email);
+    cy.get('[type="password"]')
+        .type(password).should('have.value', password);   
+    cy.get('[type="submit"]')
+        .click();
+});
