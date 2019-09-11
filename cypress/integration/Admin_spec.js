6fb1ab9d1f5f5a95a0ff71cd521d82b7872a88b5
bpeterson1663 <bpeterson1663@gmail.com>
2019-09-10 20:27:03 -0500

    Add delete test along with the create test

--- cypress/integration/Admin_spec.js
+++ cypress/integration/Admin_spec.js
@@ -3,7 +3,7 @@ describe('Admin Functionality', () => {
         cy.visit("http://localhost:3000/login");
         cy.login(Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
     });
-    it.only('should create new retro', () => {
+    it('should create new retro', () => {
         cy.get('[name="retro_name"]')
             .type('New Retro')
         cy.get('[name="retro_vote"]')
@@ -19,4 +19,18 @@ describe('Admin Functionality', () => {
             })
             .should('be.visible');
     });
+    it('should be delete retro and warning message should be shown', () => {
+        cy.get('[data-id="delete_button"]').first().click();
+        cy.get('[data-id="warning_dialog"]').should('be.visible');
+        cy.get('[data-id="cancel-delete_button"]').click();
+        cy.get('[data-id="warning_dialog"]').should('not.be.visible');
+
+        cy.get('[data-id="delete_button"]').first().click();
+        cy.get('[data-id="warning_dialog"]').should('be.visible');
+        cy.get('[data-id="confirm-delete_button"]').click();
+        cy.get('[data-id="snackbar_message"]', (el) => {
+            expect(el).to.have.css('background-color', '#43A047')
+        })
+        .should('be.visible');
+    }) 
 });
